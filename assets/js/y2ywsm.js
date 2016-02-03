7a4f4cb0c5070fdf837f5f8fad452c13de561d87
PITG-support <support@partner-it-group.com>
2016-02-03 10:57:33 +0000

    Disapear date

--- assets/js/y2ywsm.js
+++ assets/js/y2ywsm.js
@@ -7,16 +7,43 @@
 
 (function($) {
     $(document).ready(function(){
-        
-        
        $("#delivery_date").parent().append('<div style="display:none;" id="dtBox"></div>');
+        
         
         $('#delivery_date').attr('data-field', 'datetime');
         $( ".dtBox" ).DateTimePicker();
         $("#dtBox").DateTimePicker({
             dateFormat: 'dd/mm/yy',
-            minDate: 0
+            minDate: 0,
+            language: "fr",
         });
+        
+        if($('#shipping_method_0_you2you').is(':checked')){
+            $("#delivery_date").css("display","block");
+            $("#delivery_date").parent().find('label[for=delivery_date]').css("display","block");
+        }
+        else
+        {
+            $("#delivery_date").css("display","none");
+            $("#delivery_date").parent().find('label[for=delivery_date]').css("display","none");
+        };
+        
+        $(document).on('click', "input[name='shipping_method[0]']", function(){
+            if($(this).val() === 'You2You')
+            {
+                $("#delivery_date").parent().find('label[for=delivery_date]').css("display","block");
+                $("#delivery_date").css("display","block");
+                $('html, body').animate({
+                    scrollTop: $("#delivery_date").offset().top-100
+                }, 1000);
+            }
+            else
+            {
+                $("#delivery_date").css("display","none");
+                $("#delivery_date").parent().find('label[for=delivery_date]').css("display","none");
+            }
+        });
+            
         //$( "#delivery_date" ).DateTimePicker();
         
         /*if($('#shipping_method_0_you2you').is(':checked')){
