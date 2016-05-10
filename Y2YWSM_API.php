620d179b9223a14dadb47b2a66ee784ea77c8e38
PITG-support <support@partner-it-group.com>
2016-05-10 17:53:41 +0000

    Removed cUrl and changed to WP_HTTP

--- Y2YWSM_API.php
+++ Y2YWSM_API.php
@@ -1,14 +1,19 @@
 <?php
 
+if (!class_exists('WP_Http'))
+    include_once( ABSPATH . WPINC . '/class-http.php' );
+
+
 if (!class_exists('Y2YWSM_API')) {
 
     class Y2YWSM_API {
 
         protected $host = 'localhost:9050/api';
-        protected $curl;
+        protected $request;
         protected $url = '';
         protected $method = '';
-        protected $params;
+        protected $get_params = '';
+        protected $body_params = array();
         private $api_key;
         private $api_secret;
         protected $protocol = 'http';
@@ -17,7 +22,7 @@ if (!class_exists('Y2YWSM_API')) {
             $this->api_key = $api_key;
             $this->api_secret = $api_secret;
 
-            $this->curl = curl_init();
+            $this->request = new WP_Http();
 
             $defaults = array(
                 'protocol' => 'http',
@@ -28,65 +33,35 @@ if (!class_exists('Y2YWSM_API')) {
 
             $this->protocol = $options['protocol'];
             $this->host = $options['host'];
-
-            // Switch cURL to not worry about SSL certificate checking.
-            curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
-        }
-        
-        public function close(){
-            curl_close($this->curl);
-            $this->curl = null;
-            
-            return true;
         }
 
         public function get($url, $params = array()) {
             $this->method = 'GET';
 
             $this->url = $url;
-            
-            $this->params = $params;
+
+            $this->get_params = $params;
 
             return $this->exec();
         }
-        
-        public function post($url, $params = array()){
+
+        public function post($url, $params = array()) {
             $this->method = 'POST';
-            
+
             $this->url = $url;
-            
-            $this->params = $params;
-            
+
+            $this->body_params = $params;
+
             return $this->exec();
         }
-        
-        public function put($url, $params = array()){
+
+        public function put($url, $params = array()) {
             $this->method = 'PUT';
-            
+
             $this->url = $url;
-            
-            $this->params = $params;
-            
-            return $this->exec();
-        }
-        
-        public function patch($url, $params = array()){
-            $this->method = 'PATCH';
-            
-            $this->url = $url;
-            
-            $this->params = $params;
-            
-            return $this->exec();
-        }
-        
-        public function delete($url, $params = array()){
-            $this->method = 'DELETE';
-            
-            $this->url = $url;
-            
-            $this->params = $params;
-            
+
+            $this->body_params = $params;
+
             return $this->exec();
         }
 
@@ -94,123 +69,66 @@ if (!class_exists('Y2YWSM_API')) {
 
             if (empty($this->method) || empty($this->url)) {
                 return array(
-                    
                 );
             }
 
-            switch ($this->method) {
-                case 'GET':
-                case 'get':
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: GET'));
-                    $getParams = null;
-                    if (!empty($this->params)) {
-                        foreach ($this->params as $field_name => $field_value) {
-                            $getParams .= $field_name . '=' . urlencode($field_value) . '&';
-                        }
-
-                        curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl() . '&' . $getParams);
-                    } else {
-                        curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl());
-                    }
-                    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'GET');
-                    break;
-
-                case 'POST':
-                case 'post':
-                    $params = json_encode($this->params);
-
-                    #$string = $params['params'];
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: POST'));
-                    curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl());
-                    curl_setopt($this->curl, CURLOPT_POST, true);
-                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $params);
-                    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'POST');
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/json',
-                        'Content-Length: ' . strlen($params))
-                    );
-                    break;
-
-                case 'PUT':
-                case 'put':
-                    $params = json_encode($this->params);
-                    
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: PUT'));
-                    curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl());
-                    curl_setopt($this->curl, CURLOPT_POST, true);
-                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $params);
-                    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/json',
-                        'Content-Length: ' . strlen($params))
-                    );
-                    break;
-                
-                case 'PATCH':
-                case 'patch':
-                    $params = json_encode($this->params);
-                    
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: PATCH'));
-                    curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl());
-                    curl_setopt($this->curl, CURLOPT_POST, true);
-                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $params);
-                    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/json',
-                        'Content-Length: ' . strlen($params))
-                    );
-                    break;
-                
-                case 'DELETE':
-                case 'delete':
-                    $params = json_encode($this->params);
-                    
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: DELETE'));
-                    curl_setopt($this->curl, CURLOPT_URL, $this->buildUrl());
-                    curl_setopt($this->curl, CURLOPT_POST, true);
-                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, $params);
-                    curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-                    curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/json',
-                        'Content-Length: ' . strlen($params))
-                    );
-                    break;
-            }
+            $url = $this->buildUrl();
+            $params = $this->getDefaultParams();
+            $result = $this->request->request($url, $params);
             
-            curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-
             return array(
-                'result' => json_decode(curl_exec($this->curl)),
-                'response_code' => curl_getinfo($this->curl, CURLINFO_HTTP_CODE),
-                'errno' => curl_errno($this->curl),
-                'error' => curl_error($this->curl)
+                'result' => (isset($result['body']) ? $result['body'] : ''),
+                'response' => (isset($result['response']) ? $result['response'] : self::getDefaultResponse())
             );
         }
 
+        private function getDefaultParams() {
+            return array(
+                'method' => $this->method,
+                'body' => $this->body_params,
+                'headers' => array('api_key' => $this->api_key,
+                    'api_secret' => $this->api_secret
+                )
+            );
+        }
+
+        private static function getDefaultResponse() {
+            return array('code' => 500, 'message' => 'Server Error');
+        }
+
         private function buildUrl() {
-            $url = $this->protocol . '://' . $this->host . '/' . $this->url . '?api_secret=' . $this->api_secret . '&api_key=' . $this->api_key;
+            $url = $this->protocol . '://' . $this->host . '/' . $this->url;
+
+            if (!empty($this->get_params)) {
+                $getParams = "";
+                foreach ($this->get_params as $field_name => $field_value) {
+                    if ($getParams != "") {
+                        $getParams .= '&';
+                    }
+                    $getParams .= $field_name . '=' . urlencode($field_value);
+                }
+
+                $url .= '?' . $getParams;
+            }
+
+
             //$url = $this->protocol . '://' . $this->host . '/' . $this->url . '?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwicm9sZSI6Ikluc2NyaXQiLCJpYXQiOjE0NTQ0MzIxNTAsImV4cCI6MTQ1NDQzNTc1MH0.fBGNYzaNzqsd_e78DcXDHnzT8FFIFLFrMm6ATVl_eKI';
             return $url;
         }
-        
-        public function test_connection(){
+
+        public function test_connection() {
             return true;
-            if(empty($this->api_secret) || empty($this->api_key)){
+            if (empty($this->api_secret) || empty($this->api_key)) {
                 return false;
             }
-            
+
             $result = $this->get('auth/test');
             
-            if(in_array($result['response_code'], array(401, 403, 404, 500))){
+            if (in_array($result['response']['code'], array(401, 403, 404, 500))) {
                 return false;
             }
-            
-            if($result['errno'] != 0){
-                return false;
-            }
-            
+
             return true;
-            
         }
 
     }
